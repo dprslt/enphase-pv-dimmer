a5cab2a37b77ccc9e7d68263f8423212f706b0e0
Theo Depresle <theo.depresle@gmail.com>
2023-01-09 18:00:38 +0100

    Source dotenv for local run

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 
+ import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+ dotenv.config()
+ 
  import got from 'got'
 import { MeterReading, MeterReadings } from './types/IvpMetersReadings.js'
 
