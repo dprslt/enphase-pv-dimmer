153cd150c55f92b4bb5672bd965f81bbcd140151
dprslt <theo.depresle@gmail.com>
2023-11-12 18:11:53 +0100

    fix new format for dimmer state

--- src/ports-adapters/dimmer/wifi-dimmer.ts
+++ src/ports-adapters/dimmer/wifi-dimmer.ts
@@ -1,15 +1,27 @@
-import { timeHttpGetText } from '../http.js';
+import { timeHttpGetJson, timeHttpGetText } from '../http.js';
 import { Dimmer } from './dimmer.js';
+
+
+type WifiDimmerStatePayload = {
+        "dimmer": number,
+        "temperature": string,
+        "power": number,
+        "Ptotal": number,
+        "alerte": number,
+        "relay1": number,
+        "relay2": number,
+        "minuteur": boolean
+}
 
 export class WifiDimmer implements Dimmer {
     constructor(private dimmerHostname: string) {}
 
     async readValues(): Promise<{ perc?: number | undefined; temp?: number | undefined }> {
-        const result = await timeHttpGetText(`http://${this.dimmerHostname}/state`);
-        const [textPerc, textTemp] = result.split(';');
+        const dimmerState = await timeHttpGetJson<WifiDimmerStatePayload>(`http://${this.dimmerHostname}/state`);
+        
         return {
-            perc: textPerc ? Number.parseInt(textPerc) : undefined,
-            temp: textTemp ? Number.parseInt(textTemp) : undefined,
+            perc: dimmerState.dimmer || undefined,
+            temp: dimmerState.temperature ? Number.parseInt(dimmerState.temperature) : undefined,
         };
     }
     async modulePower(power: number): Promise<void> {
